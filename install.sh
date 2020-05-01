733e9f1715cceb310cbc02f0b16c811efd0c667f
RZ <30381083+w311ang@users.noreply.github.com>
2020-05-01 23:50:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ LDFLAGS=" -lm" pip install pillow
 git clone https://github.com/NyanChanMeow/SSRSpeed && cd SSRSpeed
 pip install -r requirements.txt
 echo 'python main.py --skip-requirements-check $@' > main.sh && chmod 700 main.sh
-echo '使用"./main.sh"代替"python main.py"'
+echo '安装已完成，请使用"./main.sh"代替"python main.py"'
