190518890f98f5481d8750360afff15ee7d5bf59
RZ <30381083+w311ang@users.noreply.github.com>
2020-05-01 23:30:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-pkg install -y libjpeg-turbo clang python git libsodium freetype && pip install --upgrade pip wheel && LDFLAGS=" -lm" pip install pillow && git clone https://github.com/NyanChanMeow/SSRSpeed && cd SSRSpeed && pip install -r requirements.txt && echo 'python main.py --skip-requirements-check $@' >> main.sh && chmod 700 main.sh && echo '使用"./main.sh"代替"python main.py"'
+set -e
+pkg install -y libjpeg-turbo clang python git libsodium freetype
+pip install --upgrade pip wheel && LDFLAGS=" -lm" pip install pillow
+git clone https://github.com/NyanChanMeow/SSRSpeed && cd SSRSpeed
+pip install -r requirements.txt
+echo 'python main.py --skip-requirements-check $@' >> main.sh && chmod 700 main.sh
+echo '使用"./main.sh"代替"python main.py"'
