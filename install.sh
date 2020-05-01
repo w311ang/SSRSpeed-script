41ef5d2d124600b5b27b9aee5800e6fd62843af2
RZ <30381083+w311ang@users.noreply.github.com>
2020-05-02 01:34:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ git clone https://github.com/NyanChanMeow/SSRSpeed && cd SSRSpeed
 pip install -r requirements.txt
 echo 'python main.py --skip-requirements-check $@' > main.sh && chmod 700 main.sh
 echo 'python web.py --skip-requirements-check $@' > web.sh && chmod 700 web.sh
-echo '安装已完成，请使用指令"./main.sh"或"./web.sh"，而非"python main.sh"或"python web.py"'
+echo '安装已完成，请使用指令"./main.sh"或"./web.sh"，而非"python main.py"或"python web.py"'
